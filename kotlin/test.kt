a99a0e38ada54727296f90bf7956fc6f9c6e9673
kangwlgns <kangwlgns@o.cnu.ac.kr>
2023-03-06 19:02:46 +0900

    first

--- kotlin/test.kt
+++ kotlin/test.kt
@@ -1,8 +1,3 @@
 fun main() = System.`in`.bufferedReader().run {
-    println("         ,r'\"7")
-    println("r`-_   ,'  ,/")
-    println(" \\. \". L_r'")
-    println("   `~\\/")
-    println("      |")
-    println("      |")
+    
 }
